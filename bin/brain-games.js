e140275238e06652566433aa6ee997967fd530e2
ilia.m <ilmksmv@gmail.com>
2024-02-07 13:45:30 +0100

    refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
 import greetings from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
 greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const greetings = () => {
-	let userName = readlineSync.question('May I have your name? ');
-	return console.log('Hi, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hi, ${userName}!`);
 };
 export default greetings;
