3bb72fcc25202237ae344655e4d75dd04f3a32d6
ilia.m <ilmksmv@gmail.com>
2024-02-16 18:25:23 +0100

    added upd logic for brain-even game

---
+++ src/games/brain-even-logic.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const isEvenGameLogic = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const isEven = (randomNumber) => randomNumber % 2 === 0;
+  let correctAnswersCount = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (
+      (isEven(randomNumber) && userAnswer.toLowerCase() === 'yes')
+			|| (!isEven(randomNumber) && userAnswer.toLowerCase() === 'no')
+    ) {
+      correctAnswersCount += correctAnswersCount + 1;
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was ${
+          isEven(randomNumber) ? 'yes' : 'no'
+        }.`,
+      );
+      return console.log(`Let's try again, ${userName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+export default isEvenGameLogic;
