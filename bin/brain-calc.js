cac958aa243685b7cf218c46ac6c12408e898b62
ilia.m <ilmksmv@gmail.com>
2024-02-22 17:28:14 +0100

    create executable file for brain-calc game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import isCorrectResultExpression from '../src/games/brain-calc-logic.js';
+import runBrainCalc from '../src/games/brain-calc-logic.js';
 
-isCorrectResultExpression();
+runBrainCalc();
