e0a5c65df62b375d337dadeff7f08b8bd79d8c73
ilia.m <ilmksmv@gmail.com>
2024-02-22 17:30:32 +0100

    wrote game logic for the brain-calc

--- src/games/brain-calc-logic.js
+++ src/games/brain-calc-logic.js
@@ -1,25 +1,39 @@
-import app from '../index.js';
-
-/*
-Сами же игры по сути должны только генерировать данные
-и запускать движок с общей логикой, передавая в него свои данные,
-и уже потом обрабатывать эти данные
-
-*/
+import gameEngine from '../index.js';
 
 // правила игры
 const gameRules = () => 'What is the result of the expression?';
 
-const createQuestion = () => {
+// создание данных для игры
+const createTaskData = () => {
   // генерируем случаные числа для выражений
-  const randomNumber1 = Math.floor(Math.random() * 100);
-  const randomNumber2 = Math.floor(randomNumber1 / 10);
+  const number1 = Math.floor(Math.random() * 100);
+  const number2 = Math.floor(number1 / 10);
 
-  const operations = ['+', '-', '*'];
+  // массив со знаками для выражений
+  const operations = ['-', '+', '*'];
 
-  for (let i = 0; i < operations.length; i += 1) {}
+  // генерируем выбор случайного элемента массива
+  const randomIndex = Math.floor(Math.random() * operations.length);
+  const randomOperationIndex = operations[randomIndex];
+
+  // формируем вопрос со сгенерированным варажением
+  const askQuestion = `Question: ${number1} ${randomOperationIndex} ${number2}`;
+
+  // считаем результат выражения
+  let result = 0;
+
+  if (randomOperationIndex === '+') {
+    result = number1 + number2;
+  } else if (randomOperationIndex === '-') {
+    result = number1 - number2;
+  } else if (randomOperationIndex === '*') {
+    result = number1 * number2;
+  }
+
+  /* делаем числовой результат строкой,
+  чтобы корректно сравнивать его с ответом пользователя */
+  const resultStr = result.toString();
+  return [askQuestion, resultStr];
 };
 
-const resultOfExpression = () => {};
-
-export default () => app(gameRules, createQuestion, resultOfExpression);
+export default () => gameEngine(gameRules, createTaskData);
