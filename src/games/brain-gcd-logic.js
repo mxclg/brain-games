4b699395b6e677b79f9a241be5505f20dacd9580
ilia.m <ilmksmv@gmail.com>
2024-02-26 13:56:40 +0100

    wrote code with game logic

---
+++ src/games/brain-gcd-logic.js
@@ -0,0 +1,44 @@
+import gameEngine from '../index.js';
+
+// правила игры
+const gameRules = () => 'Find the greatest common divisor of given numbers.';
+
+// создание данных для игры
+const createTaskData = () => {
+  // генерируем случайные числа для выражений
+  let number1 = Math.floor(Math.random() * 100);
+  let number2 = Math.floor(Math.random() * 100);
+
+  // формируем вопрос со сгенерированными числами
+  const askQuestion = `Question: ${number1} ${number2}`;
+
+  let remainderOfDivide = number1 % number2;
+  let result = 0;
+
+  /* условная конструкция помогает определить, какое число больше,
+  чтобы правильно сформировать деление */
+  if (number1 > number2) {
+  /* вычисляем НОД по методу Евклида – делим меньшее число на остаток от дедления,
+  пока остаток от дедления не станет равен 0 */
+    while (remainderOfDivide !== 0) {
+      number1 = number2;
+      number2 = remainderOfDivide;
+      remainderOfDivide = number1 % number2;
+    }
+    result = number2;
+  } else {
+    remainderOfDivide = number2 % number1;
+    while (remainderOfDivide !== 0) {
+      number2 = number1;
+      number1 = remainderOfDivide;
+      remainderOfDivide = number2 % number1;
+    }
+    result = number1;
+  }
+  /* делаем числовой результат строкой,
+  чтобы корректно сравнивать его с ответом пользователя */
+  const resultStr = result.toString();
+  return [askQuestion, resultStr];
+};
+
+export default () => gameEngine(gameRules, createTaskData);
