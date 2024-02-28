87a880a8adea971eb7d792eeb308b91091f95c3c
ilia.m <ilmksmv@gmail.com>
2024-02-28 16:22:30 +0100

    changed the way of import

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import isEvenGameLogic from '../src/games/brain-even-logic.js';
+import runBrainEven from '../src/games/brain-even-logic.js';
 
-isEvenGameLogic();
+runBrainEven();
