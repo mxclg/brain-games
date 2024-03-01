6934687412b668ab13ae4d7f1116b288bf03d626
ilia.m <ilmksmv@gmail.com>
2024-03-01 15:44:20 +0100

    took out code with greeting phraze from executable file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import greetings from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${userName}!`);
 };
