86eeb769d94fae45840b081eb07ee421fb0c6b77
ilia.m <ilmksmv@gmail.com>
2024-03-01 13:44:00 +0100

    renamed file

--- src/randomNumbers.js
+++
@@ -1,5 +0,0 @@
-export const getRandomFromOneToHundred = () => Math.floor((Math.random() * 100) + 1);
-
-export const getRandomFromTwoToHundred = () => Math.floor((Math.random() * 99) + 2);
-
-export const getRandomFromOneToQuarter = () => Math.floor((Math.random() * 25) + 1);
