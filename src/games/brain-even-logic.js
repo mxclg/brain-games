dbb8720debbf9a03ba60a9bf1cd571fd2fc07185
ilia.m <ilmksmv@gmail.com>
2024-03-04 13:15:47 +0100

    created separate function which show result

--- src/games/brain-even-logic.js
+++ src/games/brain-even-logic.js
@@ -5,19 +5,14 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (randomNumber) => randomNumber % 2 === 0;
 
+const isEvenResult = (randomNumber) => (isEven(randomNumber) === true ? 'yes' : 'no');
+
 const createTaskData = () => {
   const randomNumber = getRandomNumber();
 
   const askQuestion = `Question: ${randomNumber}`;
 
-  let result = '';
-  if (isEven(randomNumber) === true) {
-    result = 'yes';
-  } else {
-    result = 'no';
-  }
-
-  const resultStr = result.toString();
+  const resultStr = isEvenResult(randomNumber).toString();
   return [askQuestion, resultStr];
 };
 
